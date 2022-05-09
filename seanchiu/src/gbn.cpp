a7882f8241a2631a1b5bcca05b52421be9bc193e
sean <schiu8958@bths.edu>
2022-05-09 12:41:02 -0400

    asdqwe

--- seanchiu/src/gbn.cpp
+++ seanchiu/src/gbn.cpp
@@ -102,6 +102,7 @@ void A_input(struct pkt packet)
         }
       } else {
         stoptimer(0);
+        printf("Send base : %d\n", send_base);
         while(in_transit[send_base] != NULL && send_base != packet.acknum + 1) {
           free(in_transit[send_base]);
           in_transit[send_base] = NULL;
