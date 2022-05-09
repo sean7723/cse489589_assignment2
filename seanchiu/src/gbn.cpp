98338fdd0c5921c9141dae23f6380b279f556d2e
sean <schiu8958@bths.edu>
2022-05-09 14:46:32 -0400

    dsadqwe

--- seanchiu/src/gbn.cpp
+++ seanchiu/src/gbn.cpp
@@ -120,10 +120,6 @@ void A_input(struct pkt packet)
             next_packet->checksum = next_packet->seqnum + next_packet->acknum + payload_checksum;
             tolayer3(0, *next_packet);
             in_transit[next_seq_num] = next_packet;
-            if(send_base == next_seq_num) {
-              //printf("Attempting to start timer after!\n");
-              starttimer(0, TIMEOUT);
-            }
             next_seq_num = (next_seq_num + 1) % WINDOW_SIZE;
             buffer.pop();
           }
